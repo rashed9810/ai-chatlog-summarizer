b506b55271fb90afd61b0b4b9a12c38c664879d4
rashed9810 <rashed.cs@gmail.com>
2025-05-21 13:23:26 +0600

    Initial setup with imports and stop words list

---
+++ summarizer.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import os
+import argparse
+import string
+from collections import Counter
+
+class ChatLogSummarizer:
+
+    def __init__(self):
+        """Initialize the summarizer with default settings."""
+        self.user_messages = []
+        self.ai_messages = []
+        self.all_messages = []
+        # Common English stop words
+        self.stop_words = {
+            'i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you',
+            'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself',
+            'she', 'her', 'hers', 'herself', 'it', 'its', 'itself', 'they', 'them',
+            'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this',
+            'that', 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been',
+            'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing',
+            'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until',
+            'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between',
+            'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to',
+            'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again',
+            'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how',
+            'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some',
+            'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too',
+            'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now', 'would',
+            'could', 'please', 'like', 'help'
+        }
+
+    
